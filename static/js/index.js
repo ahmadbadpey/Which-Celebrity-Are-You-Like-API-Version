18da0ee40cc48ec9b527311f5cc4040047b18d56
Ahmad <ahmadbadpey@gmail.com>
2023-02-08 15:09:43 +0330

    some min changes

--- static/js/index.js
+++ static/js/index.js
@@ -13,7 +13,6 @@ let myDropzone = new Dropzone("#upload_form", {
         getPredict(file.name)
         // console.log(file.name)
     }
-
 });
 
 function getPredict(image) {
@@ -28,86 +27,6 @@ function getPredict(image) {
     })
         .then(r => r.text())
         .then(response => {
-            console.log(response)
-            // const title = response.toLowerCase()
-
-            /*getBestTitle(title)
-                .then(result => {
-                    const endpoint = "https://en.wikipedia.org/w/api.php";
-
-                    const parameters = {
-                        action: "query",
-                        titles: result, //Person Name that comes from getBestTitle function
-                        prop: "images",
-                        iiprop: "url",
-                        format: "json",
-                        origin: "*"
-                    };
-
-                    fetch(endpoint + "?" + new URLSearchParams(parameters))
-                        .then(response => response.json())
-                        .then(data => {
-                            // console.log(data)
-                            const pageId = Object.keys(data.query.pages)[0];
-                            const images = data.query.pages[pageId].images;
-
-                            console.table(images)
-                            console.log(result)
-
-
-                            // Filter the images contains person name and Exclude irrelevant one
-                            const filteredImages = images.filter(image => {
-                                const title = image.title.toLowerCase();
-                                result = result.toLowerCase();
-
-                                return title.includes("file:" + result) || title.includes("file:" + result.replace(/ /gi,''));
-                            });
-                            console.table(filteredImages)
-
-                            // Get the image URLs
-                            const imageUrls = filteredImages.map(image => {
-                                const imageTitle = image.title.replace("File:", "");
-                                console.log("imageTitle : " + imageTitle)
-                                return `https://en.wikipedia.org/wiki/Special:Redirect/file/${encodeURIComponent(imageTitle)}`;
-                            });
-
-                            console.table(imageUrls)
-
-                            let cel_image_url =  imageUrls[0]
-                            let cel_title = result;
-
-                            document.querySelector('#cel_image').src = cel_image_url;
-                            document.querySelector('#cel_title').innerHTML = cel_title
-
-                        })
-                        .catch(error => {
-                            console.log("API request failed", error);
-                        });
-                })*/
-
+            // console.log(response)
         })
-}
-
-//This function get
-const getBestTitle = async (title) => {
-    const endpoint = "https://en.wikipedia.org/w/api.php";
-    const parameters = {
-        action: "query",
-        list: "search",
-        srsearch: title,
-        format: "json",
-        origin: "*"
-    };
-    let best_title = null;
-
-    await fetch(endpoint + "?" + new URLSearchParams(parameters))
-        .then(response => response.json())
-        .then(data => {
-            best_title = data.query.search[0].title
-        })
-        .catch(reason => {
-            console.log(reason)
-        })
-
-    return best_title;
 }
